0d76ebb84a23531c48f4d54e31f8b82b2b23c50a
im_Ddvid_ <david.michael15@yahoo.com.ph>
2018-09-10 08:26:33 -0700

    CONNECTION CONFIGURATION
    NEW FORM, DATABASE CONFIGURATION

--- ClassSchedulingComputerAided/ClassSchedulingComputerAided/Program.cs
+++ ClassSchedulingComputerAided/ClassSchedulingComputerAided/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using ClassSchedulingComputerAided.Properties;
 
 namespace ClassSchedulingComputerAided
 {
@@ -16,7 +18,68 @@ namespace ClassSchedulingComputerAided
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin());
+
+            string server = Settings.Default["Server"].ToString();
+            string port = Settings.Default["Port"].ToString();
+            string dbName = Settings.Default["DatabaseName"].ToString();
+            string usrDb = Settings.Default["UsernameDB"].ToString();
+            string pwdDb = Settings.Default["PasswordDB"].ToString();
+
+            if (Settings.Default["Server"].ToString() != "" &&
+               Settings.Default["Port"].ToString() != "" &&
+               Settings.Default["DatabaseName"].ToString() != "" &&
+               Settings.Default["UsernameDB"].ToString() != "")
+            {
+                string sqlConnection = "server=" + server
+                     + "; username=" + usrDb
+                     + "; password=" + pwdDb
+                     + "; port=" + port + ";";
+
+                if (DBExists(sqlConnection, dbName) == true)
+                {
+                    Application.Run(new frmLogin());
+                }
+                else
+                {
+                    Application.Run(new frmConnectionWizard());
+                }
+
+            }
+            else
+            {
+                Application.Run(new frmConnectionWizard());
+            }
+                
+        }
+
+        public static bool DBExists(string conn, string dbName)
+        {
+            bool isExists = false;
+            try
+            {
+                using (MySqlConnection dbconn = new MySqlConnection(conn))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand("SELECT COUNT(*) FROM information_schema.schemata WHERE SCHEMA_NAME= @dbName;", dbconn))
+                    {
+                        isExists = false;
+                        cmd.Parameters.AddWithValue("@dbName", dbName);
+                        dbconn.Open();
+                        cmd.ExecuteNonQuery();
+                        MySqlDataReader dr = cmd.ExecuteReader();
+                        if (dr.Read())
+                        {
+                            if (Convert.ToInt32(dr["COUNT(*)"].ToString()) > 0)
+                                isExists = true;
+                        }
+                        dbconn.Close();
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message, "DBexists");
+            }
+            return isExists;
         }
     }
 }
